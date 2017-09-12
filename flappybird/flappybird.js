ad416a9f493743ceedd6311c291c8a0883e38dd7
Tim Schlechter <timschlechter@gmail.com>
2017-09-12 11:13:14 +0200

    Update flappybird.js

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -15,39 +15,39 @@ var state = {
         this.background.width = game.width;
         this.background.height = game.height;
 
-        // // Vogel
+        // Vogel
         this.bird = game.add.sprite(100, 245, 'bird');
         game.physics.arcade.enable(this.bird);
         this.bird.body.gravity.y = 1000;
         this.bird.animations.add('fly');
-        // this.bird.checkWorldBounds = true;
-        // this.bird.events.onOutOfBounds.add(this.hit, this);
+        this.bird.checkWorldBounds = true;
+        this.bird.events.onOutOfBounds.add(this.hit, this);
 
-        // // Spatiebalk roept de functie 'flap' aan
+        // Spatiebalk roept de functie 'flap' aan
         var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.flap, this);  
 
-        // // Punten op 0 zetten en het label aanmaken
-        // this.points = 0;
-        // this.labelPoints = game.add.text(20, 20, "0", {
-        //     font: "30px Arial",
-        //     fill: "#ffffff"
-        // });        
+        // Punten op 0 zetten en het label aanmaken
+        this.points = 0;
+        this.labelPoints = game.add.text(20, 20, "0", {
+            font: "30px Arial",
+            fill: "#ffffff"
+        });        
 
         // Maak iedere 3 seconden een nieuwe buis aan
         this.timer = game.time.events.loop(3000, this.createPipe, this);
 
-        // // Verhoog iedere seconde de score
-        // this.timer = game.time.events.loop(1000, this.score, this);
+        // Verhoog iedere seconde de score
+        this.timer = game.time.events.loop(1000, this.score, this);
 
-        // // Geluiden aan het spel toevoegen
-        // this.flapSound = game.add.audio('flap');
-        // this.hitSound = game.add.audio('hit');
+        // Geluiden aan het spel toevoegen
+        this.flapSound = game.add.audio('flap');
+        this.hitSound = game.add.audio('hit');
     },
 
     update: function () {
         // Als de vogel een buis raakt, wordt de functie 'hit' uitgevoerd
-        //game.physics.arcade.overlap(this.bird, this.pipes, this.hit, null, this);
+        game.physics.arcade.overlap(this.bird, this.pipes, this.hit, null, this);
     },
 
     flap: function() {
